d9b701229f3e2fd7b1a3e2c759a042e90d2c3643
Jason Irish <jirish@dynamicdoes.com>
2013-01-22 19:11:47 -0600

    config tweak

--- _config.php
+++ _config.php
@@ -1,6 +1,6 @@
 <?php
 
-Object::add_extension('SiteConfig', 'ShareSiteConfig');
+//Object::add_extension('SiteConfig', 'ShareSiteConfig');
 
 // add this to your main _config.php file for each page you'd like to include icons
 //Object::add_extension('Page', 'ShareExtension');
